4ab902032564880c13ee87df3e797a4c66b48e56
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-06-26 10:00:34 -0400

    adds AspnetcoreService project

---
+++ Images/AspnetcoreService/Controllers/HelloNetCoreController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AspnetcoreService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloNetCoreController : ControllerBase
+    {
+        // GET: api/<HelloNetCoreController>
+        [HttpGet]
+        public string Get()
+        {
+            return $"Hello from ASP.NET Core App in { Environment.MachineName }";
+        }
+    }
+}
